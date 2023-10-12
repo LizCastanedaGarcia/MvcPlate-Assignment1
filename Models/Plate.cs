dd85ad2323dc295f36f6fc948fd15a00b331cdc7
LizCastanedaGarcia <122821054+LizCastanedaGarcia@users.noreply.github.com>
2023-10-12 12:42:41 -0400

    Add a data model class

---
+++ Models/Plate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPlate.Models
+{
+    public class Plate
+    
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StockDate { get; set; }
+
+        public string Description { get; set; }
+        public string Origen { get; set; }
+        public string Color { get; set; }
+        public decimal Size { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Rating { get; set; }
+    }
+}
