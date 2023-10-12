75cdd8a3d3d7f33282dc9d78a0cf73826a2cf3d6
LizCastanedaGarcia <122821054+LizCastanedaGarcia@users.noreply.github.com>
2023-10-12 12:51:19 -0400

    Create a database context class

--- Data/MvcPlateContext.cs
+++ Data/MvcPlateContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcPlate.Models;
 
 namespace MvcPlate.Data
 {
-    public class MvcPlateContext
+    public class MvcPlateContext : DbContext
     {
+        public MvcPlateContext(DbContextOptions<MvcPlateContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Plate> Plate { get; set; }
     }
 }
